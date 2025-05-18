21e0dc283026f703c497371348a17a81fe7778da
Coder-X15 <samrubenabraham@gmail.com>
2025-05-18 09:51:31 +0530

    Modified source

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,17 +9,29 @@ class StreamlitUIUpdater:
         self.model = model.Model()
 
     def mainloop(self):
-        query = st.chat_input("What's on your mind? Drop it off in here :)", key = "Input")
-        response = self.model.invoke(query)
-        if query is not None:
-            with st.chat_message("user"):
-                st.write(query)
-        if response is not None:
-            with st.chat_message("bot"):
-                st.write(response)
-        else:
-            with st.chat_message("bot"):
-                st.write("Error generating message.")
+        # initialize session variable `messages``
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        # print messages from chat history (i.e., the `messages` variable)
+        for message in st.session_state.messages:
+            with st.chat_message(message['role']):
+                st.write(message['content'])
+
+        # activates the query-response system upon user prompt
+        if query := st.chat_input("What's on your mind? Drop it off in here :)", key = "Input"):
+
+            # step 1: make markdown version of user query and add it to chat history
+            st.chat_message("user").markdown(query)
+            st.session_state.messages.append({"role": "user", "content":query})
+            
+
+            # step 2: generate response
+            response = self.model.invoke(query)
+
+            # step 3: add response to chat history
+            st.chat_message("assistant").markdown(response)
+            st.session_state.messages.append({"role":"assistant","content":response})
 
 if __name__ == "__main__":
     app = StreamlitUIUpdater()
